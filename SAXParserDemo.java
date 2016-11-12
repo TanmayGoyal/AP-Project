e637f43c0fab03a7ba8ad4e1f0004231a7679e06
Subbu Dantu <subbudantu@Subbus-MacBook-Pro.local>
2016-11-12 15:30:47 +0530

    Parsed the file and created first GUI window and also added dblp.dtd

--- SAXParserDemo.java
+++ SAXParserDemo.java
@@ -1,15 +1,21 @@
-import java.io.File;
+import org.xml.sax.InputSource;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
+import java.io.*;
 
 public class SAXParserDemo {
 	public static void main(String[] args) {
 		try {
 			File inputFile = new File("dblp.xml");
+			InputStream inputStream = new FileInputStream(inputFile);
+			Reader reader = new InputStreamReader(inputStream);
+			InputSource is = new InputSource(reader);
+			is.setEncoding("ISO-8859-1");
+			System.setProperty("jdk.xml.entityExpansionLimit", "0");
 			SAXParserFactory factory = SAXParserFactory.newInstance();
         	SAXParser saxParser = factory.newSAXParser();
         	ArticleHandler articlehandler = new ArticleHandler();
-        	saxParser.parse(inputFile, articlehandler);
+        	saxParser.parse(is, articlehandler);
 		}
 		catch (Exception e) {
         	e.printStackTrace();
